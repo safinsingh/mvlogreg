9fe358ab63346e9c3ffdda8edafaee4d343581fe
Safin Singh <safin.singh@gmail.com>
2024-05-17 13:04:00 -0700

    plot

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
+def inv_sigmoid(sig):
+    return -np.log(1/sig - 1)
+
 def logistic_error(X_train, y_train, w, b):
     m = X_train.shape[0]
 
@@ -46,6 +49,7 @@ def main():
     itns = 10000
     alpha = 1e-1
     epsilon = 1e-6
+    threshold = 0.5
     error_history = []
 
     w, b = gradient_descent(X_train, y_train, np.zeros(X_train.shape[1]), 0, alpha)
@@ -65,6 +69,25 @@ def main():
     plt.xlabel("Iterations")
     plt.ylabel("J(w,b)")
     plt.plot(np.arange(len(error_history)) + 1, error_history)
+    plt.show()
+
+    # Plot threshold line
+    z = inv_sigmoid(threshold)
+    plt.title("Logistic Regression")
+    plt.xlabel("x_1")
+    plt.ylabel("x_2")
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train)
+    # For two features:
+    #   z = w_1 * x_1 + w_2 * x_2 + b
+    #   0 = w_1 * x_1 + w_2 * x_2 + b - z
+    #   -(w_2 * x_2) = w_1 * x_1 + b - z
+    #   x_2 = -(w_1 * x_1 + b - z)/w_2
+    #   x_2 = (z - b - w_1 * x_1)/w_2
+    def calculate_x2(x1):
+        return (z - b - w[0] * x1) / w[1]
+    x_tmp = np.array([-10, 10])
+    y_tmp = calculate_x2(x_tmp)
+    plt.plot(x_tmp, y_tmp, label=f"{threshold}={w}x+b")
     plt.show()
 
 if __name__ == "__main__":
