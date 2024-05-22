b2a0b7c859a83df5b5c2624a94c3f1680874f66e
Safin Singh <safin.singh@gmail.com>
2024-05-21 18:10:59 -0700

    regularization & vectorization

--- main.py
+++ main.py
@@ -7,62 +7,56 @@ def sigmoid(z):
 def inv_sigmoid(sig):
     return -np.log(1/sig - 1)
 
-def logistic_error(X_train, y_train, w, b):
+def logistic_error(X_train, y_train, w, b, lambda_):
     m = X_train.shape[0]
+    z = np.dot(X_train, w) + b
+    f = sigmoid(z)
+    cost = -np.mean(y_train * np.log(f) + (1 - y_train) * np.log(1 - f))
+    regularization_cost = (lambda_ / (2 * m)) *  np.sum(w ** 2)
 
-    cost = 0.0
+    return cost + regularization_cost
 
-    for i in range(m):
-        z = np.dot(X_train[i], w) + b
-        f = sigmoid(z)
-        cost += y_train[i] * np.log(f) + (1 - y_train[i]) * np.log(1 - f)
-    
-    return -cost / m
-
-def calculate_gradient(X_train, y_train, w, b):
+def calculate_gradient(X_train, y_train, w, b, lambda_):
     m = X_train.shape[0]
-    dj_dw = np.zeros(w.shape[0])
-    dj_db = 0
+    z = np.dot(X_train, w) + b
+    f = sigmoid(z)
+    diff = f - y_train
 
-    for i in range(m):
-        z = np.dot(X_train[i], w) + b
-        f = sigmoid(z)
-        diff = f - y_train[i]
-
-        for j in range(w.shape[0]):
-            # jth parameter in ith data row
-            dj_dw[j] += diff * X_train[i, j]
-        dj_db += diff
+    dj_dw = (np.dot(X_train.T, diff) + lambda_ * w) / m
+    dj_db = np.mean(diff)
     
-    return dj_dw / m, dj_db / m
+    return dj_dw, dj_db
 
-def gradient_descent(x_train, y_train, w, b, alpha):
-    dj_dw, dj_db = calculate_gradient(x_train, y_train, w, b)
-    return w - alpha * dj_dw, b - alpha * dj_db
+def gradient_descent(X_train, y_train, w, b, alpha, lambda_, num_iters, epsilon):
+    error_history = []
+
+    for _ in range(num_iters):
+        dj_dw, dj_db = calculate_gradient(X_train, y_train, w, b, lambda_)
+
+        w -= alpha * dj_dw
+        b -= alpha * dj_db
+        
+        error = logistic_error(X_train, y_train, w, b, lambda_)
+        if error_history and abs(error - error_history[-1]) < epsilon:
+            break
+        error_history.append(error)
+    
+    return w, b, error_history
 
 
 def main():
     X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
     y_train = np.array([0, 0, 0, 1, 1, 1])
 
-    
-    itns = 10000
+    num_iters = 10000
     alpha = 1e-1
     epsilon = 1e-6
     threshold = 0.5
-    error_history = []
+    lambda_ = 0.7
 
-    w, b = gradient_descent(X_train, y_train, np.zeros(X_train.shape[1]), 0, alpha)
-    error_history.append(logistic_error(X_train, y_train, w, b))
-
-    for _ in range(itns):
-        w, b = gradient_descent(X_train, y_train, w, b, alpha)
-        sse = logistic_error(X_train, y_train, w, b)
-        sse_diff = abs(sse - error_history[-1])
-
-        if sse_diff < epsilon:
-            break
-        error_history.append(sse)
+    w = np.zeros(X_train.shape[1])
+    b = 0
+    w, b, error_history = gradient_descent(X_train, y_train, w, b, alpha, lambda_, num_iters, epsilon)
 
     print(f"logistic regression eqn.: y=1/(1+e^-({w}x+{b}))")
     plt.title("Learning Curve")
